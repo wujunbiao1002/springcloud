82c1f371d57e04b7b9d4b8cdac6b59b666c6e84b
Arjun <wujunbiao1002@qq.com>
2022-09-04 00:17:31 +0800

    feat：config bus

---
+++ cloud-config-client-3355/src/main/java/com/wjb/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,25 @@
+package com.wjb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <b><code>${NAME}</code></b>
+ * <p/>
+ * Description
+ * <p/>
+ * <b>Creation Time:</b> ${DATE} ${TIME}.
+ *
+ * @author Arjun
+ * @version 1.0.0
+ * @since ${PROJECT_NAME} ${PROJECT_VERSION}
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/wjb/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,26 @@
+package com.wjb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <b><code>${NAME}</code></b>
+ * <p/>
+ * Description
+ * <p/>
+ * <b>Creation Time:</b> ${DATE} ${TIME}.
+ *
+ * @author Arjun
+ * @version 1.0.0
+ * @since ${PROJECT_NAME} ${PROJECT_VERSION}
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
